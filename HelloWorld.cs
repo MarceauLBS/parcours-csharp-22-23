1f5983d4654fa68da42c63c4014880cb72adf0e9
marceau <marceau.labasse@ynov.com>
2022-10-04 10:37:11 +0200

    Ex1 Q1

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class HelloWorld_Exercice
+    {
+        public static string HelloWorld()
+        {
+            string message = "Hello World !";
+            return message;
+        }
+    }
+}
